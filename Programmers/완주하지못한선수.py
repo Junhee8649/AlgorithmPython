e42629d40e56b8fa465e9bf6eb552acdf6cb7fd2
Junhee-lee <sk5514224@gmail.com>
2026-05-27 12:34:18 +0900

    solve 완주하지 못한 선수

---
+++ Programmers/완주하지못한선수.py
@@ -0,0 +1,10 @@
+from collections import Counter
+
+
+def solution(participant, completion):
+    p = Counter(participant)
+    c = Counter(completion)
+
+    for i in p:
+        if p[i] != c[i]:
+            return i
