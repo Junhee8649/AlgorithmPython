f99c84f9b5dc4d90e8d125fa019934fff17f0041
Junhee-lee <sk5514224@gmail.com>
2026-06-30 11:16:40 +0900

    solve 콜라츠 추측

---
+++ Programmers/콜라츠 추측.py
@@ -0,0 +1,17 @@
+def solution(num):
+    if num == 1:
+        return 0
+    answer = 0
+    isFinish = False
+    for i in range(500):
+        if num % 2 == 0:
+            num //= 2
+        else:
+            num = 3 * num + 1
+        answer += 1
+        if num == 1:
+            isFinish = True
+            break
+    if not isFinish:
+        answer = -1
+    return answer
